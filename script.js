b8afcb033b7ace09158cd80050ada8b637683dc8
Natali <nana_projetti@hotmail.com>
2023-12-14 21:55:34 -0300

    criando-html-js

---
+++ script.js
@@ -0,0 +1,39 @@
+function adicionarTarefa() {
+    const inputTarefa = document.getElementById('novaTarefa');
+    const textoTarefa = inputTarefa.value.trim();
+    
+    if(textoTarefa !== "") {
+        const listaTarefas = document.getElementById("listaTarefas");
+
+        listaTarefas.innerHTML += `
+            <li>
+                <span>${textoTarefa}</span>
+                <button onclick="marcarConcluida(this)">Concluída</button>
+                <button onclick="removerTarefa(this)">Remover</button>
+            </li>
+        `
+    }
+}
+
+function marcarConcluida(botaoConcluir){
+    const tarefa = botaoConcluir.parentNode;
+
+    //Adiciona a classe "completed" para mudar o estilo
+    tarefa.classList.toggle("completed");
+
+    if(tarefa.classList.contains(completed)){
+        //Move a tarefa para final da lista
+        document.getElementById("listaTarefas").appendChild(tarefa);
+
+        //Remove os botões da tarefa concluída
+        const botoes = tarefa.querySelectorAll("button");
+        botoes.Remover.parentNode.remove();
+         
+    }
+}
+
+function removerTarefa(botaoRemover) {
+    botaoRemover.parentNode.remove();
+    
+    
+    }
